5f1df7a6e9db244ceca44b20573cbe2f86ef9431
jooho <111959400+jooho2055@users.noreply.github.com>
2023-08-19 01:46:55 -0700

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // To make the color change of header when user scrolls down
+
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 
@@ -11,9 +12,23 @@ document.addEventListener('scroll', () => {
 });
 
 // To make change of home opacity when user scrolls
+
 const homeContainer = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
+const homeHeight = homeContainer.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
 	homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// To hide the arrow up button when the page is at the top
+
+const arrow = document.querySelector('.arrow-up');
+
+arrow.style.opacity = 0;
+document.addEventListener('scroll', () => {
+	if (window.scrollY > homeHeight / 2) {
+		arrow.style.opacity = 1;
+	} else {
+		arrow.style.opacity = 0;
+	}
 });
